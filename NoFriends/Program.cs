f068df083e71ab3a473cea1a33bbdbf93c95d0d6
iLinked1337 <43618132+iLinked1337@users.noreply.github.com>
2019-11-22 18:08:08 +0100

    spaghetti code begone!
    removed a bunch of dupe code, and also jenz: it's never good to stop the websocket's event dispatching

--- NoFriends/Program.cs
+++ NoFriends/Program.cs
@@ -11,60 +11,46 @@ namespace test
 {
     class Program
     {
+        private static bool _block;   
+        
 
         static void Main()
         {
             Console.Title = "NoFriends";
             Console.WriteLine("Choose Your Desired Removal.\n[1] = Remove All Incoming Friends \n[2] = Block All Incoming Friends");
-            string type = Console.ReadLine();
-            if (type == ("1"))
-            {
-                Console.Clear();
-                Console.WriteLine("Welcome! Enter token:");
-                string token = Console.ReadLine();
-                DiscordSocketClient client = new DiscordSocketClient();
-                client.OnLoggedIn += Client_OnLoggedIn;
-                client.Login(token);
-                Thread.Sleep(-1);
-            }
-            if (type == ("2"))
-            {
-                Console.Clear();
-                Console.WriteLine("Welcome! Enter token:");
-                string token = Console.ReadLine();
-                DiscordSocketClient client = new DiscordSocketClient();
-                client.OnLoggedIn += Client_OnLoggedInn;
-                client.Login(token);
-                Thread.Sleep(-1);
-            }
 
+            if (Console.ReadLine() == ("2"))
+                _block = true;
+
+            Console.Clear();
+            Console.WriteLine("Welcome! Enter token: ");
+            DiscordSocketClient client = new DiscordSocketClient();
+            client.OnLoggedIn += Client_OnLoggedIn;
+            client.Login(Console.ReadLine());
+            
+            Thread.Sleep(-1);
         }
 
         private static void Client_OnLoggedIn(DiscordSocketClient client, LoginEventArgs args)
         {
-            Console.Clear();
-            Console.WriteLine("Press Enter When Ready");
-            Console.ReadLine();
-            foreach (var relationship in args.Relationships)
+            Task.Run(() =>
             {
-                if (relationship.Type == RelationshipType.IncomingRequest)
-                    relationship.Remove();
-                Console.WriteLine("Removed! You May Now Close The Program.");
-            }
+                Console.Clear();
+                Console.WriteLine("Press Enter When Ready");
+                Console.ReadLine();
+                foreach (var relationship in args.Relationships)
+                {
+                    if (relationship.Type == RelationshipType.IncomingRequest)
+                    {
+                        if (_block)
+                            relationship.User.Block();
+                        else
+                            relationship.Remove();
+                    }
+                        
+                    Console.WriteLine("Removed! You May Now Close The Program.");
+                }
+            });
         }
-
-        private static void Client_OnLoggedInn(DiscordSocketClient client, LoginEventArgs args)
-        {
-            Console.Clear();
-            Console.WriteLine("Press Enter When Ready");
-            Console.ReadLine();
-            foreach (var relationship in args.Relationships)
-            {
-                if (relationship.Type == RelationshipType.IncomingRequest)
-                    relationship.User.Block();
-                Console.WriteLine("Blocked! You May Now Close The Program.");
-            }
-        }
-
     }
 }
